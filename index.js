ef86d3f00e7742be4656072a9ca898a0f1d5960b
Koala Lu <monaneyadav.200@gmail.com>
2023-10-02 08:27:46 +0000

    add logic and templete in js

---
+++ index.js
@@ -0,0 +1,95 @@
+const downloadButton = document.getElementById('download-button');
+downloadButton.addEventListener('click', () => {
+  const resumeData = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    phone: document.getElementById('phone').value,
+    address: document.getElementById('address').value,
+    school: document.getElementById('school').value,
+    degree: document.getElementById('degree').value,
+    major: document.getElementById('major').value,
+    graduationDate: document.getElementById('graduation-date').value,
+    company: document.getElementById('company').value,
+    title: document.getElementById('title').value,
+    startDate: document.getElementById('start-date').value,
+    endDate: document.getElementById('end-date').value,
+    skill1: document.getElementById('skill-1').value,
+    skill2: document.getElementById('skill-2').value,
+    skill3: document.getElementById('skill-3').value,
+    reference1: document.getElementById('reference-1').value,
+    reference2: document.getElementById('reference-2').value,
+    reference3: document.getElementById('reference-3').value,
+  };
+  const resumeTemplate = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Resume</title>
+      <style>
+        body {
+          background-color: #fff;
+          font-family: sans-serif;
+        }
+    
+        h1 {
+          font-size: 24px;
+          font-weight: bold;
+          margin-bottom: 10px;
+        }
+    
+        ul {
+          list-style-type: none;
+          padding: 0;
+        }
+    
+        li {
+          margin-bottom: 10px;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>${resumeData.name}</h1>
+      <ul>
+        <li>Email: ${resumeData.email}</li>
+        <li>Phone: ${resumeData.phone}</li>
+        <li>Address: ${resumeData.address}</li>
+      </ul>
+      <h2>Education</h2>
+      <ul>
+        <li>School: ${resumeData.school}</li>
+        <li>Degree: ${resumeData.degree}</li>
+        <li>Major: ${resumeData.major}</li>
+        <li>Graduation Date: ${resumeData.graduationDate}</li>
+      </ul>
+      <h2>Work Experience</h2>
+      <ul>
+        <li>Company: ${resumeData.company}</li>
+        <li>Title: ${resumeData.title}</li>
+        <li>Start Date: ${resumeData.startDate}</li>
+        <li>End Date: ${resumeData.endDate}</li>
+      </ul>
+      <h2>Skills</h2>
+      <ul>
+        <li>${resumeData.skill1}</li>
+        <li>${resumeData.skill2}</li>
+        <li>${resumeData.skill3}</li>
+      </ul>
+      <h2>References</h2>
+      <ul>
+        <li>${resumeData.reference1}</li>
+        <li>${resumeData.reference2}</li>
+        <li>${resumeData.reference3}</li>
+      </ul>
+    </body>
+    </html>
+  `;
+  const resumeBlob = new Blob([resumeTemplate], { type : 'text/html' });
+  const resumeUrl = URL.createObjectURL(resumeBlob);
+  const resumeLink = document.createElement('a');
+  resumeLink.href = resumeUrl;
+  resumeLink.download = 'resume.html';
+  resumeLink.click();
+  URL.revokeObjectURL(resumeUrl);
+})
